22465455af0815a38f7a6d4d71f1d28e23f23ba3
Alex Ronet <sierra@alexronet.x10.mx>
2011-11-01 18:27:30 -0700

    Fixed message return typos and added the Ident option to fwee.conf.

--- modules/nickserv.py
+++ modules/nickserv.py
@@ -20,37 +20,43 @@ def sendNickServ(evname, net, message):
 	if not net.name in sendPass: sendPass[net.name] = 0
 	
 	#log.edebug("sendPass="+str(sendPass[net.name]))		#Displays sendPass for debugging purposes.
-	if sendPass[net.name] is 1: return
+	if sendPass[net.name] == 2: return
 	
 	#log.edebug("NICKSERV:"+str(config.get("Networks/"+net.name+"/NickServ")))		#Displays nickserv config entry for debugging purposes.
 	
 	if config.get("Networks/"+net.name+"/NickServ") is None:
 		consoleMessage(net.name, "No NickServ entry found in config for "+net.name+" - Bypassing NickServ module.")
-		sendPass[net.name] = 1
-		return
-	
-	if config.get("Networks/"+net.name+"/NickServ") is None:
-		consoleMessage(net.name, "No NickServ entry found in config for "+net.name+" - Bypassing NickServ module.")
-		sendPass[net.name] = 1
+		event.trigger("Network/Event/AutoJoin", net)
+		sendPass[net.name] = 2
 		return
 	
 	if config.get("Networks/"+net.name+"/NickServ") == "":
 		console.Message(net.name, "NickServ entry for "+net.name+" is blank. - Bypassing NickServ module.")
-		sendPass[net.name] = 1
+		event.trigger("Network/Event/AutoJoin", net)
+		sendPass[net.name] = 2
 		return
 	
 	chkMessage = str(message).split(":",1)
 	if len(chkMessage) < 2: return
 	chkMessage = str(chkMessage[1]).split("!",1)
 	#log.edebug("chkMessage="+str(chkMessage[0]))		#Displays chkMessage for debugging purposes.
+	
 	if str(chkMessage[0]) == "NickServ":
-		consoleMessage(net.name, "NickServ entry found in config.json for "+net.name)
+		if sendPass[net.name] == 1:
+			event.trigger("Network/Event/AutoJoin", net)
+			return
+		
+		consoleMessage(net.name, "NickServ entry found in fwee.conf for "+net.name)
 		consoleMessage(net.name, "Sending NickServ password...")
 		net.send("PRIVMSG NickServ :"+config.get("Networks/"+net.name+"/NickServ")+"\r\n")
 		sendPass[net.name] = 1
 	
 	if str(chkMessage[0]) == "AuthServ":
-		consoleMessage(net.name, "NickServ entry found in config.json for "+net.name)
+		if sendPass[net.name] == 1:
+			event.trigger("Network/Event/AutoJoin", net)
+			return
+		
+		consoleMessage(net.name, "NickServ entry found in fwee.conf for "+net.name)
 		consoleMessage(net.name, "Sending AuthServ password...")
 		net.send("PRIVMSG AuthServ :"+config.get("Networks/"+net.name+"/NickServ")+"\r\n")
 		sendPass[net.name] = 1
