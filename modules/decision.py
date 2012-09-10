7ca3ad4445431cf12387ad49397769280bd48b81
Alec Elton <alec.elton@gmail.com>
2012-09-09 23:31:58 -0500

    Bugfixes in decision module (list index issues when no options are given)

--- modules/decision.py
+++ modules/decision.py
@@ -10,4 +10,9 @@ def cleanup():
 	event.unlisten(decide)
 
 def decide(evname, net, message, args):
-	message.reply("%s: %s"%(message.fromnick.split("!")[0], " ".join(args).rstrip("?").split(" or ")[random.randint(0,9999)&1]))
+	fromNick=message.fromnick.split("!")[0]
+	opts=" ".join(args).rstrip("?").split(" or ")
+	if len(opts)<2:
+		message.reply("%s: need more options!"%(fromNick,))
+	else:
+		message.reply("%s: %s"%(fromNick, opts[random.randint(0,9999)&1]))
